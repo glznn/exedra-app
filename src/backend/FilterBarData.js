12c94f4d3e4bdb486a2afd5463235d252f5bec29
Glenn Montepiedra <glennmontepiedra@gmail.com>
2025-03-31 15:33:42 -0700

    Added FilterBarData.js which contains icons that will be used to filter characters.

---
+++ src/backend/FilterBarData.js
@@ -0,0 +1,34 @@
+import { CiStar } from "react-icons/ci";
+
+export const ElementData = [
+    "/exedra-icons/elements/aqua-elem.png",
+    "/exedra-icons/elements/dark-elem.png",
+    "/exedra-icons/elements/flame-elem.png",
+    "/exedra-icons/elements/forest-elem.png",
+    "/exedra-icons/elements/light-elem.png",
+    "/exedra-icons/elements/void-elem.png",
+]
+
+export const RoleData = [
+    "/exedra-icons/role/attacker.png",
+    "/exedra-icons/role/breaker.png",
+    "/exedra-icons/role/buffer.png",
+    "/exedra-icons/role/debuffer.png",
+    "/exedra-icons/role/defender.png",
+    "/exedra-icons/role/healer.png",
+]
+
+export const RarityData= [
+    {
+        rarity: 5,
+        icon: <CiStar />
+    },
+    {
+        rarity:4,
+        icon: <CiStar />
+    },
+    {
+        rarity:3,
+        icon: <CiStar />
+    }    
+]
