6b11dcdd796d1f95c0c988cb145c37cf2fdf3dd5
Glenn Montepiedra <glennmontepiedra@gmail.com>
2025-03-30 19:11:55 -0700

    Added search bar front end and functionality to character screen.

--- src/CharacterScreen.js
+++ src/CharacterScreen.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { CharacterData_five } from './backend/CharacterData_five'
 import { CharacterData_four } from './backend/CharacterData_four'
 import { CharacterData_three } from './backend/CharacterData_three'
@@ -5,33 +6,31 @@ import './CharacterScreen.css'
 
 function CharacterScreen() {
 
+  const [filter, setFilter] = useState('');
+
+  function filterByName(e) {
+    setFilter(e.target.value.toUpperCase());
+  }
+
+  const filteredData = [
+    ...CharacterData_five,
+    ...CharacterData_four,
+    ...CharacterData_three,
+  ].filter((character) =>
+    character.name.toUpperCase().includes(filter)
+  );
+  
+  
   return (
     <div className="CharacterScreen">
       <img className="char__banner" src="https://madoka-exedra.com/assets_teaser2/img/teaser/ver_black/kv_black_pc.jpg">
         </img>
         <h1>Characters<br></br></h1>
-        {CharacterData_five.map((character, index) => (
+        <input type="text" id="userInput" 
+        autoComplete='off'
+        onChange={filterByName} placeholder="Search.."></input>
+        {filteredData.map((character, index) => (
           <div className="char__card" key={index}>
-            <img className="char__type" src={"/exedra-icons/elements/" + character.type + "-elem.png"}></img>
-            <img className="char__role" src={"/exedra-icons/role/" + character.role + ".png"}></img>
-            <div className="charimg__container">
-            <img src={character.image} className="char__image" />
-            </div>
-          </div>
-        ))}
-        {CharacterData_four.map((character, index) => (
-          <div className="char__card" key={index}>
-          <img className="char__type" src={"/exedra-icons/elements/" + character.type + "-elem.png"}></img>
-          <img className="char__role" src={"/exedra-icons/role/" + character.role + ".png"}></img>
-          <div className="charimg__container">
-          <img src={character.image} className="char__image" />
-          </div>
-        </div>
-        ))}
-        {CharacterData_three.map((character, index) => (
-          <div className="char__card" key={index}>
-            <div className="char__text">
-            </div>
             <img className="char__type" src={"/exedra-icons/elements/" + character.type + "-elem.png"}></img>
             <img className="char__role" src={"/exedra-icons/role/" + character.role + ".png"}></img>
             <div className="charimg__container">
